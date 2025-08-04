3aac7cf06ada5c5a069b04d61b6f5e95ade9b2f0
Zakaria <zakig701@gmail.com>
2025-08-04 03:29:35 +0100

    Added constants.

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color myPurple = Color.fromARGB(255, 117, 38, 163);
+  static const Color myYellow = Color.fromARGB(255, 241, 199, 61);
+}
---
+++ lib/constants/strings.dart
@@ -0,0 +1,8 @@
+// Routing
+const String homeScreen = '/';
+const String characterDetailsScreen = '/details';
+
+// Services
+const String baseUrl = 'https://api.openf1.org/v1';
+const String getDriversUrl = '/drivers?session_key=7763';
+const String url = '$baseUrl/$getDriversUrl';
